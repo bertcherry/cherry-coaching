4fe479a4d0febcb06e293d4b5230075e04b54517
bertcherry <119577715+bertcherry@users.noreply.github.com>
2024-02-07 16:14:37 -0800

    video display as component and style

---
+++ src/components/Video/Video.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Stream } from "@cloudflare/stream-react";
+import Nav from '../Nav/Nav';
+import styles from './Video.module.css';
+
+const Video = () => {
+    const [video, setVideo] = useState({});
+    const { id } = useParams();
+
+    useEffect(() => {
+        const getVideo = async () => {
+            const resp = await fetch(`/api/video/${id}`);
+            const videoResp = await resp.json();
+            setVideo(videoResp);
+        };
+
+        getVideo();
+    }, [id]);
+
+    if (!Object.keys(video).length) return <div />;
+
+    return (
+        <div className={styles.container}>
+            <Nav />
+            <div className={styles.videoDisplay}>
+                <h1>{video.name}</h1>
+                <div className={styles.videoContainer}>
+                    <Stream controls src={video.id} />
+                </div>
+                <p className={styles.videoDescription}>{video.description}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Video;
