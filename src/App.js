7e27a76397ac218549b5a5e73dfe85fe6aaa6284
bertcherry <119577715+bertcherry@users.noreply.github.com>
2024-02-07 17:11:39 -0800

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-
+import Home from './components/Home/Home';
 import Articles from './components/articles';
 import Article from './components/article';
 import Video from './components/Video/Video';
 
+
 function App() {
   return (
     <Routes>
+      <Route path='/' element={<Home />} />
       <Route path='/articles' element={<Articles />} />
       <Route path='/articles/:id' element={<Article />} />
       <Route path='/videos/:id' element={<Video />} />
