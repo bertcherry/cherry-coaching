5cc4e28eeeebf882e03c546b9d02a7b746b9cf53
bertcherry <119577715+bertcherry@users.noreply.github.com>
2024-02-07 17:30:17 -0800

    add about page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
+import About from './components/About/About';
 import Articles from './components/articles';
 import Article from './components/article';
 import Video from './components/Video/Video';
@@ -9,6 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
       <Route path='/articles' element={<Articles />} />
       <Route path='/articles/:id' element={<Article />} />
       <Route path='/videos/:id' element={<Video />} />
