032874ca324473fa50bf456861d7f1a9865a860c
bertcherry <119577715+bertcherry@users.noreply.github.com>
2024-11-19 14:04:20 -0800

    add videos route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About/About';
 import Articles from './components/articles';
 import Article from './components/article';
 import Video from './components/Video/Video';
+import Videos from '.components/Video/Videos';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path='/articles' element={<Articles />} />
         <Route path='/articles/:id' element={<Article />} />
         <Route path='/videos/:id' element={<Video />} />
+        <Route path='/videos' element={<Videos />} />
       </Routes>
     </div> 
   );
