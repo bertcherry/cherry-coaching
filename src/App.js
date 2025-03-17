a5836adf7c441d04a1b2b52e2cdbe8092c87ec7b
bertcherry <119577715+bertcherry@users.noreply.github.com>
2025-03-17 12:54:45 -0700

    revert last

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import Nav from './components/Nav/Nav';
 import Router from './components/Router/Router';
-import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
-  const helmetContext = {};
   return (
-    <HelmetProvider context={helmetContext}>
       <div className='app'>
         <Nav />
         <Router />
       </div> 
-    </HelmetProvider>
   );
 }
 
